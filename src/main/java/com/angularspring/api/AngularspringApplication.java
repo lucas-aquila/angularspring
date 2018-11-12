c83ad063492a6baaa18398260e54cdf64629e1aa
Lucas <lucas-aquila@outlook.com>
2018-11-12 11:01:42 -0200

    Criação do projeto

---
+++ src/main/java/com/angularspring/api/AngularspringApplication.java
@@ -0,0 +1,12 @@
+package com.angularspring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularspringApplication.class, args);
+	}
+}
