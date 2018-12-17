16702f8f419f8937ffe2949e9d06668155464b89
Lucas Aquila <lucas-aquila@outlook.com>
2018-12-17 15:41:10 -0200

    Validando valores inválidos com Bean Validation

--- src/main/java/com/angularspring/api/resource/CategoriaResource.java
+++ src/main/java/com/angularspring/api/resource/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -40,7 +41,7 @@ public class CategoriaResource {
 	 * @return
 	 */
 	@PostMapping
-	public ResponseEntity<Categoria> insert(@RequestBody Categoria categoria, HttpServletResponse response) {
+	public ResponseEntity<Categoria> insert(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = this.categoriaRepository.save(categoria);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
