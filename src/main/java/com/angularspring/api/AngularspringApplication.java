8a79079e84112514bd7d761a15b823bea58dced0
Lucas Aquila <lucas-aquila@outlook.com>
2019-01-14 11:38:09 -0200

    Implementação segurança com OAuth2 - Mais ou menos kk

--- src/main/java/com/angularspring/api/AngularspringApplication.java
+++ src/main/java/com/angularspring/api/AngularspringApplication.java
@@ -1,7 +1,9 @@
 package com.angularspring.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @SpringBootApplication
 public class AngularspringApplication {
@@ -9,4 +11,11 @@ public class AngularspringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AngularspringApplication.class, args);
 	}
+	
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("admin").roles("ROLE");
+	}
+
 }
