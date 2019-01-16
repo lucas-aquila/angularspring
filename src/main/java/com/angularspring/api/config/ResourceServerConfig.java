f093e65aaf56927bb1a7cd17f7c45f8db596f359
Lucas Aquila <lucas-aquila@outlook.com>
2019-01-16 12:28:42 -0200

    Movendo usuário para o banco de dados

--- src/main/java/com/angularspring/api/config/ResourceServerConfig.java
+++ src/main/java/com/angularspring/api/config/ResourceServerConfig.java
@@ -2,24 +2,30 @@ package com.angularspring.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("admin").password("{noop}admin").roles("ROLE");
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 
-    @Value("${resource.id:spring-boot-application}")
+	@Value("${resource.id:spring-boot-application}")
     private String resourceId;
     
 
@@ -31,6 +37,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		 	.and()
 		 	.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 		 	.csrf().disable();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
